2b1106b69b9f2d6905ee1235a2bb907375d3a845
ravsa <rvsbana@gmail.com>
2015-10-25 12:52:38 +0530

    login with gui

--- content.py
+++ content.py
@@ -2,14 +2,16 @@ from gi.repository import Notify
 from bs4 import BeautifulSoup
 import mechanize
 import cPickle
-import os
+import re
+from simplecrypt import decrypt
+import os,sys
 import time
 update_time = 10
+retry_time = 1
 creds, notification, message, request, online, browser, cookies, soup = None, None, None, None, None, None, None, None
 with open(os.path.abspath('creds'), 'r') as file:
     creds = cPickle.load(file)
-
-
+creds=[decrypt('email',creds[0]),decrypt('password',creds[1])]
 def init():
     global browser, cookies
     Notify.init('idontknow')
@@ -21,9 +23,10 @@ def init():
 
 
 def content():
-    global browser, creds, notifications, messages, request, online, soup
+    global browser, creds, notification, message, request, online, soup
     init()
     while True:
+        time.sleep(retry_time)
         try:
             browser.open('https://m.facebook.com')
             browser.select_form(nr=0)
@@ -31,18 +34,64 @@ def content():
             browser.form['pass'] = creds[1]
             res = browser.submit()
             while True:
-                browser.open('https://m.facebook.com')
-                soup = BeautifulSoup(browser.response().read())
-                
+                time.sleep(update_time)
+                try:
+                    browser.open('https://m.facebook.com')
+                    soup = BeautifulSoup(browser.response().read())
+                except:
+                    Notify.Notification.new(
+                        "<b>Authentication Failure</b>", 'Check your email and password', os.path.abspath('./icons/messages/1.png')).show()
+                    print "Error 321"
+                    continue
+                for s in soup.find('a', href=re.compile(r'.*notifications.*')):
+                    try:
+                        notification = int(
+                            re.search(r'\((.*?)\)', s.get_text()).group(1))
+                    except:
+                        try:
+                            notification = re.search(r'\((.*?)\)', s).group(1)
+                        except:
+                            pass
+
+                for s in soup.find('a', href=re.compile(r'.*messages.*')):
+                    try:
+                        message = int(
+                            re.search(r'\((.*?)\)', s.get_text()).group(1))
+                    except:
+                        try:
+                            message = int(re.search(r'\((.*?)\)', s).group(1))
+                        except:
+                            pass
+                for s in soup.find('a', href=re.compile(r'.*buddylist.*')):
+                    try:
+                        online = int(
+                            re.search(r'\((.*?)\)', s.get_text()).group(1))
+                    except:
+                        try:
+                            online = int(re.search(r'\((.*?)\)', s).group(1))
+                        except:
+                            pass
+                for s in soup.find('a', href=re.compile(r'.*friends.*')):
+                    try:
+                        request = int(
+                            re.search(r'\((.*?)\)', s.get_text()).group(1))
+                    except:
+                        try:
+                            request = int(re.search(r'\((.*?)\)', s).group(1))
+                        except:
+                            pass
+                lis=[notification, request, message, online]
+                print lis
+                sys.exit()
         except mechanize.URLError, e:
             Notify.uninit()
             Notify.Notification.new("<b>Disconnect</b>", 'connection error',
                                     os.path.abspath('./icons/messages/1.png')).show()
-            Notify.uninit()
             break
         except Exception, e:
+            print e
+            print "Error 123"
             Notify.Notification.new("<b>Authentication Failure</b>", 'Check your email and password',
                                     os.path.abspath('./icons/messages/1.png')).show()
-            Notify.uninit()
             break
 content()
--- login.py
+++ login.py
@@ -1,7 +1,54 @@
-from simplecrypt import encrypt
-import cPickle
-import getpass
+import gtk
+import glib
+import cPickle,os
+spn=None
 file=open('creds','w')
-data=[encrypt('email',raw_input('email: ')),encrypt('password',getpass.getpass())]
-cPickle.dump(data,file)
+def login():
+    global spn
+    def active(x,etc):
+        etc.modify_text(gtk.STATE_NORMAL,etc.get_colormap().alloc_color('black'))
+    def done():
+        elab.hide()
+        plab.hide()
+        ebox.hide()
+        spn.start()
+        spn.show()
+        glib.timeout_add_seconds(4,lambda x:gtk.main_quit(),'')
+    def process(x,y):
+                data=[elab.get_text(),plab.get_text()]
+                cPickle.dump(data,file)
+                done()
+    root=gtk.Window()
+    root.set_default_size(300,300)
+    root.connect('destroy',lambda x:gtk.main_quit())
+    spn=gtk.Spinner()
+    spn.set_size_request(30,30)
+    vbox=gtk.VBox()
+    image=gtk.Image()
+    image2=gtk.Image()
+    image2.set_size_request(120,120)
+    image.set_from_file(os.path.abspath('./icons/login.png'))
+    ebox=gtk.EventBox()
+    ebox.add(image)
+    ebox.connect('button-press-event',process)
+    image2.set_from_file(os.path.abspath('./icons/user.png'))
+    elab=gtk.Entry()
+    elab.set_text('Email')
+    elab.modify_text(gtk.STATE_NORMAL,elab.get_colormap().alloc_color('light grey'))
+    elab.connect('changed',active,elab)
+    plab=gtk.Entry()
+    plab.set_text('PassWord')
+    plab.set_visibility(False)
+    plab.connect('changed',active,plab)
+    plab.modify_text(gtk.STATE_NORMAL,plab.get_colormap().alloc_color('light grey'))
+    vbox.pack_start(image2,False,False,7)
+    vbox.pack_start(elab,False,False,7)
+    vbox.pack_start(plab,False,False,7)
+    vbox.pack_start(spn,False,False,2)
+    vbox.pack_start(ebox,False,False,20)
+    root.add(vbox)
+    root.show_all()
+    spn.hide()
+    gtk.main()
+login()
 file.close()
