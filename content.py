2b45601519c13a3b9c68df2494f0366fd4f9ef03
ravsa <rvsbana@gmail.com>
2015-10-26 20:58:37 +0530

    notification done

--- content.py
+++ content.py
@@ -34,9 +34,6 @@ def init():
         ('User-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0')]
     Notify.init('idontknow')
 
-    # ind_notification.set_menu(build_menu)
-    # ind_request.set_menu(build_menu)
-    # ind_message.set_menu(build_menu)
 
 
 def create():
